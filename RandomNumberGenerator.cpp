9a21a253941d0a6e5dc8b9bae5749feca6363f54
berksvml <berksvmli@gmail.com>
2025-10-25 12:53:44 +0200

    Random number generator upgraded

--- RandomNumberGenerator.cpp
+++ RandomNumberGenerator.cpp
@@ -5,8 +5,11 @@ int main(){
     using namespace std;
 
     srand(time(NULL));
-    int num = (rand() % 6) + 1;
-    cout << num;
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
+
+    cout << num1 << '\n' << num2 << '\n' << num3 << '\n';
 
     return 0;
 }
